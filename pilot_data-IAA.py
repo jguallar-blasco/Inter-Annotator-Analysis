054d53e10d04b3e4486bdd957698a40837efe072
Jimena <jgualla1@jhu.edu>
2021-07-21 15:14:59 -0400

    new changes

--- pilot_data-IAA.py
+++ pilot_data-IAA.py
@@ -2,54 +2,67 @@ import csv
 from collections import defaultdict
 import re
 import json
+import sys
 
+total_m = 0;
 
-prep_dict = defaultdict(list)
+def sort_annotations(file):
 
-# Compile annotator answers according to HIT
-csv_fh = open('pilot_data-Batch_863_results.csv', 'r', encoding='utf-8')
-reader = csv.DictReader(csv_fh)
+    prep_dict = defaultdict(list)
 
-for row in reader:
-    prep_dict[row['HITId']]
-    argument_spans = json.loads(row['Answer.argument_spans'])
-    for argument_span in argument_spans:
-        #print(argument_span)
-        prep_dict[row['HITId']].append(argument_span)
-        #print()
-    #print()
+    # Compile annotator answers according to HIT
+    csv_fh = open(file, 'r', encoding='utf-8')
+    reader = csv.DictReader(csv_fh)
+
+    for row in reader:
+        prep_dict[row['HITId']]
+        argument_spans = json.loads(row['Answer.argument_spans'])
+        for argument_span in argument_spans:
+            #print(argument_span)
+            prep_dict[row['HITId']].append(argument_span)
+            #print()
+
+    return prep_dict
         
 
 #print(prep_dict);
 
 # Calculate matches
-total_p = 0
-total_m = 0
-first = 0
+def calculate_matches(prep_dict, total_m):
+    first = 0;
+    for hit in prep_dict:
+        cur_arguments = prep_dict[hit]
+        for argument in cur_arguments:
+            cur_argument = argument['argument']
+            if (cur_argument[0] == 'arg1'):
+                if (first == 0):
+                    check_startToken = argument['startToken']
+                    check_endToken = argument['endToken']
+                    #total_p += 1
+                    first = 1
+                else:
+                    startToken = argument['startToken']
+                    endToken = argument['endToken']
+                    #total_p += 1
+                    if (check_startToken == startToken and check_endToken == endToken):
+                        total_m += 1
+        first = 0;
 
-for hit in prep_dict:
-    cur_arguments = prep_dict[hit]
-    for argument in cur_arguments:
-        cur_argument = argument['argument']
-        if (cur_argument[0] == 'arg1'):
-            if (first == 0):
-                check_startToken = argument['startToken']
-                check_endToken = argument['endToken']
-                total_p += 1
-                first = 1
-            else:
-                startToken = argument['startToken']
-                endToken = argument['endToken']
-                total_p += 1
-                if (check_startToken == startToken and check_endToken == endToken):
-                    total_m += 1
-    first = 0; 
+    return(total_m)
 
 # Calculate kappa
 
-Po = total_m/total_p
-Pe = ()
+def main():
+    total_m = 0;
+    prep_dict = sort_annotations(sys.argv[1])
+    total_m = calculate_matches(prep_dict, total_m)
 
-k = (Po-Pe)/(1-Pe)
-print(k)
+    print(total_m)
+    #Po = total_m/total_p
+    Pe = 0
+
+
+if __name__=="__main__":
+    main()
+
 
