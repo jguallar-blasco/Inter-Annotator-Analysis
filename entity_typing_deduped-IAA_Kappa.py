32b1d0301e374d4ee9564e7cffe0c3f1475350ca
Jimena <jgualla1@jhu.edu>
2021-07-27 15:20:35 -0400

    Changes to kappa

--- entity_typing_deduped-IAA_Kappa.py
+++ entity_typing_deduped-IAA_Kappa.py
@@ -33,54 +33,57 @@ def calculate_matches(prep_dict, agreement_dict):
         total = 0
         agreement = 0
         t1 = prep_dict[username]
+
+        #print(username)
+        #print(t1)
         agreement_dict[username] = []
         for compare_username in prep_dict:
+            if username == compare_username:
+                continue
             t2 = prep_dict[compare_username]
             
             username_dict = {'LOC': 0, 'MON': 0, 'ORG': 0, 'PER': 0, 
                     'RES': 0, 'SID': 0, 'TTL': 0, 'VAL': 0, 'VEH': 0, 
                     'WEA': 0, 'BAL': 0, 'COM': 0, 'CRM': 0, 'FAC': 0, 
-                    'GPE': 0, 'LAW': 0}
+                    'GPE': 0, 'LAW': 0, -1: 0}
             compare_username_dict = {'LOC': 0, 'MON': 0, 'ORG': 0, 'PER': 0,
                     'RES': 0, 'SID': 0, 'TTL': 0, 'VAL': 0, 'VEH': 0, 
                     'WEA': 0, 'BAL': 0, 'COM': 0, 'CRM': 0, 'FAC': 0, 
-                    'GPE': 0, 'LAW': 0}
+                    'GPE': 0, 'LAW': 0, -1: 0}
 
-            if not username == compare_username:
-                print(t1)
-                for answers in t1:
-                    answer_type = answers['type']
-                    print(answer_type)
-                    print('HIIIIIIIII')
-                    username_dict[answer_type] += 1
-                print(username_dict)
+            for answers in t1:
                     
                 for compare_answers in t2:
+                    if not compare_answers['HITId'] == answers['HITId']:
+                        continue
+
+                    #print(username)
+                    #print(compare_username)
+                    answer_type = answers['type']
+                    username_dict[answer_type] += 1
+
                     answer_type = compare_answers['type']
                     compare_username_dict[answer_type] += 1
-                print(username_dict)
 
-                user_count = []
-                for count in username_dict:
-                    user_count.append(username_dict[count])
+            user_count = []
+            for count in username_dict:
+                user_count.append(username_dict[count])
+            
+            print(user_count)
 
-                compareuser_count = []
-                for count in compare_username_dict:
-                    compareuser_count.append(compare_username_dict[count])
+            compareuser_count = []
+            for count2 in compare_username_dict:
+                compareuser_count.append(compare_username_dict[count2])
 
-                print(username)
-                print(compare_username)
-                print(cohen_kappa_score(user_count, compareuser_count))
-                        
+            print(compareuser_count)
 
-        if (total == 0):
-            pass
-        else:
-            # Calculate observed agreement for annotator
-            agreement_dict[username].append(agreement/total)
-    
-    #print(agreement_dictt)
-    return agreement_dict
+
+                #print(username)
+                #print(compare_username)
+            print(username)
+            print(compare_username)
+            print(cohen_kappa_score(user_count, compareuser_count))
+                       
 
 
 
@@ -90,17 +93,11 @@ def main():
     prep_dict = defaultdict(list)
     agreement_dict = defaultdict(list)
     prep_dict = sort_annotations(sys.argv[1], prep_dict)
-    agreement_dict = calculate_matches(prep_dict, agreement_dict)
-
-    total = 0
-    turkers = 0
-    del agreement_dict['Turkle.Username']
-    for averages in agreement_dict:
-        total += agreement_dict[averages][0]
-        turkers += 1
+    del prep_dict['Turkle.Username']
+    del prep_dict['jgualla1']
+    calculate_matches(prep_dict, agreement_dict)
 
     # Calculate average observed agreement between annotators
-    print(round((total/turkers),2))
 
     with open("entity_typing_deduped-Results.json", "w") as f1:
         json.dump(agreement_dict, f1)
