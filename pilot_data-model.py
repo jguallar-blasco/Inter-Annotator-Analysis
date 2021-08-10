b5454193f8825903c201c47187c9dfe0c46533ac
Jimena <jgualla1@jhu.edu>
2021-08-10 17:38:28 -0400

    mode file started

---
+++ pilot_data-model.py
@@ -0,0 +1,27 @@
+import csv
+
+# Prep data from results csvs
+def prep_data()
+
+    csv_fh = open(file, 'r', encoding='utf-8')
+    reader = csv.DictReader(csv_fh)
+
+# Sample each annotator
+def sample_annotators():
+# Estimate average directions
+
+# Estimate average length
+
+def random_spam_model():
+# Model to create random span
+
+# Determine if need to generate or not
+
+# Determine if before or after
+
+# Determine length
+
+# Clean-up
+
+def calculate_agreement():
+# Calculate raw agreement 
